21ce0f392bf9705d4fe823ffcc23f61517ae0659
Hector Noguera <hector.noguera03@gmail.com>
2022-02-06 16:56:36 -0430

    Modificar Servicio

---
+++ controlador/modificacion_servicio.php
@@ -0,0 +1,40 @@
+<?php  
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		require_once "modelo/ServicioEstetico.php";
+		$servicio = new ServicioEstetico;//Instanciar ServicioEstetico
+		$servicio->setId($_POST['id']);
+		$servicio->setNombre($_POST['nombre']);
+		$servicio->setTipo($_POST['tipo']);
+		$servicio->setDescripcion($_POST['descripcion']);
+		$servicio->setCosto($_POST['costo']);
+		$res = $servicio->modificar();
+		if($res) {
+		  http_response_code(200);
+	
+		  echo json_encode([
+		  'titulo' => 'Registro Modificado',
+		  'mensaje' => 'El Servicio ha sido modificado exitosamente',
+		  'tipo' => 'success'
+		  ]);    
+		} else{
+		  echo json_encode([
+		  'titulo' => 'Error',
+		  'mensaje' => $servicio->getError(),
+		  'tipo' => 'error'
+		  ]);  
+		}
+	}
+	else{
+		if (is_file("vista/".$pagina.".php")) 
+		{
+			require_once "modelo/ServicioEstetico.php";
+			$serv = new ServicioEstetico;//Instanciar clase ServicioEstetico
+			$serv->setId($_GET['id']);
+			$servicio = $serv->consultar();
+			require_once("vista/".$pagina.".php");
+		}
+		else{
+			echo "pagina en construcion";
+		}
+	}
+?>
---
+++ vista/modificacion_servicio.php
@@ -0,0 +1,64 @@
+<?php require "vista/componentes/header.php" ?>
+<!--main container start-->
+<div class="main-container">
+
+    <div class="nombre">
+        <center>
+            <h2>Registro de Servicio Estético</h2>
+        </center>
+    </div>
+    <br>
+
+    <form action="" class="formulario" id="formulario">
+        <!-- Grupo: Nombre -->
+        <div class="formulario__grupo" id="grupo__nombre">
+            <label for="nombre" class="formulario__label">Nombre del Servicio Estético</label>
+            <div class="formulario__grupo-input">
+                <input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="Nombre del Servicio Estético" value="<?= $servicio->nombre ?>">
+                <i class="formulario__validacion-estado fas fa-times-circle"></i>
+            </div>
+            <p class="formulario__input-error">El nombre del Servicio Estético tiene que ser de 2 a 16 dígitos y solo puede contener letras.</p>
+        </div>
+
+        <div class="formulario__grupo" id="grupo__costo">
+            <label for="costo" class="formulario__label">Costo del Servicio Estético</label>
+            <div class="formulario__grupo-input">
+                <input type="text" class="formulario__input" name="costo" id="costo" placeholder="10 $" value="<?= $servicio->costo ?>">
+                <i class="formulario__validacion-estado fas fa-times-circle"></i>
+            </div>
+            <p class="formulario__input-error">El costo del Servicio Estético solo puede contener numeros y caracteres especiales, tiene que ser de 1 a 10 dígitos.</p>
+        </div>
+
+        <div class="formulario__grupo" id="grupo__tipo">
+            <label for="tipo" class="formulario__label">Tipo de servicio</label>
+            <div class="formulario__grupo-input">
+                <input type="text" class="formulario__input" name="tipo" id="tipo" placeholder="Tipo de servicio Estético" value="<?= $servicio->tipo ?>">
+                <i class="formulario__validacion-estado fas fa-times-circle"></i>
+            </div>
+            <p class="formulario__input-error">El Tipo de servicio tiene que ser de 2 a 16 dígitos y solo puede contener letras.</p>
+        </div>
+
+        <div class="formulario__grupo" id="grupo__descripcion">
+            <label for="descripcion" class="formulario__label">Descripcion del Servicio Estético</label>
+            <div class="formulario__grupo-input">
+                <textarea name="descripcion" class="formulario__input" id="descripcion" placeholder="Colocar la descripcion del Servicio Estético" required=""><?= $servicio->descripcion ?></textarea>
+                <i class="formulario__validacion-estado fas fa-times-circle"></i>
+            </div>
+            <p class="formulario__input-error">La Descripcion del curso solo puede contener letras.</p>
+        </div>
+
+
+        <div class="formulario__mensaje" id="formulario__mensaje">
+            <p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
+        </div>
+        <br><br>
+        <div class="formulario__grupo formulario__grupo-btn-enviar">
+            <button type="submit" class="formulario__btn">Registrar</button>
+            <p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+        </div>
+    </form>
+</div>
+<!--main container end-->
+<script>const id = <?= $servicio->id ?>;</script>
+<script src="js/servicio.js"></script>
+<?php require "vista/componentes/footer.php" ?>
