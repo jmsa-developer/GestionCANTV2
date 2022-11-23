918968c23fb1b957b05de211d1308ee456ba0a94
william gimenez <William_mongera@hotmail.com>
2022-11-23 12:10:52 -0400

    modificacion de inicio

---
+++ app/vista/Bitacora.php
@@ -0,0 +1,22 @@
+<?php require "vista/componentes/header.php" ?>
+    <!--main container start-->
+    <div class="main-container">
+        <div class="card">
+
+            <div class="row">
+                <h1 class="display-8 text-center p-2 text-white bg-secondary">BITACORA</h1>
+            </div>
+
+           
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <!--main container end-->
+    <script src="js/usuarios_gestion.js"></script>
+    <?php require "vista/componentes/footer.php" ?>
